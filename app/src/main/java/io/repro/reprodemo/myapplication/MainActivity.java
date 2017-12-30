ceaff9c59741827893a13a328a85c6d8f695bd43
yuorimo <sasuraino12@gmail.com>
2017-12-30 21:23:02 +0900

    add com hand logic

--- app/src/main/java/io/repro/reprodemo/myapplication/MainActivity.java
+++ app/src/main/java/io/repro/reprodemo/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package io.repro.reprodemo.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //起動時にデータをクリアする
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
     }
     public void onJankenButtonTapped(View view){
         Intent intent = new Intent(this,ResultActivity.class);
